6d1b2cfb0da0145ae42512013628f1ab524ee988
djfanatix <43752712+djfanatix@users.noreply.github.com>
2021-03-30 11:09:41 +0200

    Update driver.js

--- drivers/inverter/driver.js
+++ drivers/inverter/driver.js
@@ -6,7 +6,9 @@ class InverterDriver extends Homey.Driver {
 
   onInit() {
     this.log('Solaredge Inverter driver has been initialized');
-
+    this.flowCards = {};
+        this._registerFlows();
+      }
   _registerFlows() {
     this.log('Registering flows');
 
@@ -20,38 +22,6 @@ class InverterDriver extends Homey.Driver {
     this._registerFlow('trigger', triggers, Homey.FlowCardTriggerDevice);
 
     //Register conditions
-    triggers = [
-      'isInverterDailyYield',
-      'isInverterStatus'
-    ];
-    this._registerFlow('condition', triggers, Homey.FlowCardCondition);
-
-    this.flowCards['condition.isInverterDailyYield']
-      .registerRunListener((args, state, callback) => {
-        this.log('Flow condition.isInverterDailyYield');
-        this.log(`- inverter.dailyYield: ${args.device.getCapabilityValue('meter_power')}`);
-        this.log(`- parameter yield: '${args.daily_yield}'`);
-
-        if (args.device.getCapabilityValue('meter_power') > args.daily_yield) {
-          return true;
-        } else {
-          return false;
-        }
-      });
-
-    this.flowCards['condition.isInverterStatus']
-      .registerRunListener((args, state, callback) => {
-        this.log('Flow condition.isInverterStatus');
-        this.log(`- inverter.status: ${args.device.getCapabilityValue('operational_status')}`);
-        this.log(`- parameter status: '${args.inverter_status}'`);
-
-        if (args.device.getCapabilityValue('operational_status').indexOf(args.inverter_status) > -1) {
-          return true;
-        } else {
-          return false;
-        }
-      });
-
   }
 
   _registerFlow(type, keys, cls) {
